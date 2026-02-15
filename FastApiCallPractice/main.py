6cfe722529c27fa91e3054a4be8975f213c3d461
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-02-15 21:16:02 +0800

    updated a pydantic model to main.py

--- FastApiCallPractice/main.py
+++ FastApiCallPractice/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, model_validator
+from typing import Optional
+from uuid import UUID, uuid4
 
 app = FastAPI()
-userlist = {"1": "Bob", "2": "Alice", "3":"Mark", "4": "Charlie", "5":"Kirk"}
 
 """
 Some commands to remember
@@ -10,34 +11,54 @@ Some commands to remember
 uvicorn main:app --reload (initialize a server for your api)
 curl.exe -H "Content-Type: application/json" "http:/127.0.0.1:8000/{endpoint}" 
 (simple get execution)
+
 Invoke-RestMethod -Uri "http://127.0.0.1:8000/{endpoint}" -Method 
 POST -Headers @{"Content-Type"="application/json"} 
 -Body '{"name":"RYC","id":"12"}' (Simple post execution)
 
+curl.exe --json '{\"name\":\"{nameinput}\",\"id\":\"{userid}\"}' 
+"http://127.0.0.1:8000/endpoint"
+
+curl.exe -X POST -H "Content-Type: application/json" 
+-d '{\"name\":\"RYC\",\"id\":\"12\"}' "http://127.0.0.1:8000/endpoint"
+(simple post execution)
+
+curl.exe -v --json '{\"name\":\"Test\",\"id\":\"99\"}' "http://127.0.0.1:8000/users"
+(check for headers)
 """
 
+userlist = {"1": "Bob", "2": "Alice", "3":"Mark", "4": "Charlie", "5":"Kirk"}
 class userInfo(BaseModel):
     name : str
-    id : str
+    id : Optional[str] = None 
+        
+    @model_validator(mode="after")
+    def userInList(model):
+        if model.id in userlist or model.name in userlist.values():
+            raise ValueError("User already in list")
+        return model
+        
 
 @app.get("/")
 def get_root():
     return {
         "message" : "Hello World",
-        "endpoints": ""
+        "users": userlist
         }
 
-@app.post("/users")
+@app.post("/v1/users")
 def create_users(userinfo: userInfo):
-    if userinfo.id in userlist:
-        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST, detail = "User already in the list" )
+    if userinfo.id is None:
+        print("no id")
+        userinfo.id = str(uuid4())
     else:
-        userlist[userinfo.id] = userinfo.name
-        new = userlist[userinfo.id]
-        return new
+        print("has id")
+        userinfo.id = userinfo.id
+    userlist[userinfo.id] = userinfo.name
+    return userinfo
        
 
-@app.get("/users/{userid}")
+@app.get("/v1/users/{userid}")
 def get_users(userid: str):
     try:
         username = userlist[userid]

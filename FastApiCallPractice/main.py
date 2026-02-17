606da97d5938d24bec0621de49b5e7f7f7de8b20
Minalinsky98 <121223142+minalinsky098@users.noreply.github.com>
2026-02-17 15:06:46 +0800

    finished api endpoints

--- FastApiCallPractice/main.py
+++ FastApiCallPractice/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel, model_validator
 from typing import Optional
-from posts import postLists
+from .posts import postLists
+from fastapi.openapi.utils import get_openapi
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 postList = postLists()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # dev only; see below
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class postType(BaseModel):
     title : str
     body : str
@@ -15,13 +25,12 @@ class PostAddId(postType):
     id : str = ""
         
     @model_validator(mode = "after")
-    
     def assign_id(self):
         self.id = str(len(postList)+1)
         return self
    
 @app.get("/v1/posts")
-def postget(id: Optional[str] = None):
+def postget(id: Optional[str] = None): #parameters allow parameterized endpoints
     if id:
         for posts in postList:
             if posts["id"] == id:
@@ -37,17 +46,26 @@ def postpost(post: postType):
 
 @app.get("/v1/posts/{postId}")
 def idPostGet(postId: str):
-    foundpost = None
-    for posts in postList:
-        if posts["id"] == postId:
-            foundpost = posts
-    if foundpost == None:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="post not found")
+    foundpost = findPost(postId)
+    return foundpost
+
+@app.put("/v1/posts/{postId}")
+def idPostPut(postId: str, post: postType):
+    foundpost = findPost(postId)
+    foundpost["title"] = post.title
+    foundpost["body"] = post.body
+    return foundpost
+
+@app.delete("/v1/posts/{postId}")
+def idPostDelete(postId: str):
+    foundpost = findPost(postId)
+    postindex = postList.index(foundpost)
+    postList.pop(postindex)
     return foundpost
 
 @app.get("/")
 def main_root():
-    apiDict = app.openapi()
+    apiDict = cache_openapi()
     allPaths = apiDict.get("paths", {}) #get the value of the key "path" returns an empty dict if not found
     pathkeys = {}
     for key in (allPaths):
@@ -58,3 +76,25 @@ def main_root():
         "allEndpoints": allPaths
     }
     
+def cache_openapi(): #caching openapi schema so it doesnt get the schema everytime api docs is opened
+    if app.openapi_schema:
+        return app.openapi_schema
+    app.openapi_schema = get_openapi(
+        title = "BrainrotAPI",
+        version = "6.7",
+        description= "Practice my apis lmao",
+        routes = app.routes
+    )
+    return app.openapi_schema
+
+def findPost(postId, foundpost = None):
+    for posts in postList:
+        if posts["id"] == postId:
+            foundpost = posts
+            break
+    if foundpost == None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="post not found")
+    return foundpost
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
